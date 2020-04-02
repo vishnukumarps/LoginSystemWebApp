a3ca67715dadc4cdf2a6d2d30a712015fcb9ec66
Bala Krishnan <Balakrishnan.Damodaran@outlook.com>
2020-04-02 19:52:05 +0530

    Docker

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace LoginSystemWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=db;Database=master;User=sa;Password=Your_password123;";
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
